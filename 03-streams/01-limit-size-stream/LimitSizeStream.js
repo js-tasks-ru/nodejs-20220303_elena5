ec7cb1a836419bee39253281d3e430a4ff8094b9
Elena Litvinova <ivanovaev90@gmail.com>
2022-03-13 19:56:41 +0000

    03-streams/01-limit-size-stream result

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.maxLength = options.limit;
+    this.currentLength = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.currentLength += chunk.length;
 
+    if (this.currentLength > this.maxLength) {
+      callback(new LimitExceededError());
+      return;
+    }
+    callback(null, chunk);
   }
 }
 
