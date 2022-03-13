5a546dc01089ceeba81ef199a812dbcf836bbaec
Elena Litvinova <ivanovaev90@gmail.com>
2022-03-13 23:23:44 +0000

    03-file-server-get — task finished

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+
 
 const server = new http.Server();
 
@@ -8,11 +10,28 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+  if (pathname && pathname.split('/').length > 1) {
+    res.statusCode = 400;
+    res.end('pathname cann\'t be nested');
 
+    return;
+  }
+
+  const filepath = path.join(__dirname, 'files', pathname);
   switch (req.method) {
     case 'GET':
+      const stream = fs.createReadStream(filepath);
+      stream.pipe(res);
 
+      stream.on('error', (error) => {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('file not found');
+        } else {
+          res.statusCode = 500;
+          res.end('something went wrong');
+        }
+      });
       break;
 
     default:
