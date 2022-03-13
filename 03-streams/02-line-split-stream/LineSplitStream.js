01c6c2eb82b6a6facab2ca05191767a56e11742a
Elena Litvinova <ivanovaev90@gmail.com>
2022-03-13 21:52:24 +0000

    02-line-split-stream first snapshot

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -7,9 +7,30 @@ class LineSplitStream extends stream.Transform {
   }
 
   _transform(chunk, encoding, callback) {
+    const data = chunk.toString();
+
+    if (!data.includes(os.EOL)) {
+      this.lastLineData = `${this.lastLineData || ''}${data}`;
+      callback();
+      return;
+    }
+
+    const fullData = `${this.lastLineData || ''}${data}`;
+    const lines = fullData.split(os.EOL);
+    const firstLine = lines.shift();
+
+    this.lastLineData = lines.join(os.EOL);
+    callback(null, firstLine);
   }
 
   _flush(callback) {
+    if (this.lastLineData) {
+      callback(null, this.lastLineData);
+    } else {
+      callback();
+    }
+
+    this.lastLineData = null;
   }
 }
 
