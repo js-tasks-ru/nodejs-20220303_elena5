23ef4946ef0c5f01e71baa21ce7b108bc00a725e
Elena Litvinova <ivanovaev90@gmail.com>
2022-03-19 10:27:29 +0000

    05-file-server-delete Finished

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,4 +1,4 @@
-const url = require('url');
+const fs = require('fs');
 const http = require('http');
 const path = require('path');
 
@@ -8,11 +8,31 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (pathname && pathname.split('/').length > 1) {
+    res.statusCode = 400;
+    res.end('pathname cann\'t be nested');
+    return;
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
+
+  if (!fs.existsSync(filepath)) {
+    res.statusCode = 404;
+    res.end('The file doesn\'t exists');
+    return;
+  }
 
   switch (req.method) {
     case 'DELETE':
-
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end('Unexpected error');
+          return;
+        }
+        res.statusCode = 200;
+        res.end('The file was deleted.');
+      });
       break;
 
     default:
